ada140032524bfdaeb3181e72a9867fa6687fb79
Claudio Morabito <claudio.morabito@uha.fr>
2019-11-05 09:52:59 +0100

    Deuxième commit a l'aide d'internet j'ai pu voir comment faire une mise en page avec le gridPane

---
+++ src/RegistrationFormApplication.java
@@ -0,0 +1,53 @@
+package src;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class RegistrationFormApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Formulaire d'inscription");
+
+        GridPane gridPane = miseEnPage();
+        Scene scene = new Scene(gridPane, 1600, 900);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    private GridPane miseEnPage() {
+
+        GridPane gridPane = new GridPane();
+
+        gridPane.setAlignment(Pos.CENTER);
+
+        gridPane.setPadding(new Insets(40, 40, 40, 40));
+
+        gridPane.setHgap(10);
+
+        gridPane.setVgap(10);
+
+
+        ColumnConstraints columnOneConstraints = new ColumnConstraints(100, 100, Double.MAX_VALUE);
+        columnOneConstraints.setHalignment(HPos.RIGHT);
+
+        ColumnConstraints columnTwoConstrains = new ColumnConstraints(200,200, Double.MAX_VALUE);
+        columnTwoConstrains.setHgrow(Priority.ALWAYS);
+
+        gridPane.getColumnConstraints().addAll(columnOneConstraints, columnTwoConstrains);
+
+        return gridPane;
+    }
+
+
+}
